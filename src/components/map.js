c5ab7fd3e4d928c960e55b0dc4f19ee5b5e6494e
Crowstorm <crowstormofficial@gmail.com>
2017-09-18 14:03:45 +0200

    user side works

--- src/components/map.js
+++ src/components/map.js
@@ -1,20 +1,41 @@
 
 import React from 'react';
-import {geolocated} from 'react-geolocated';
+
 
 
  class GoogleMap extends React.Component{
-    componentDidMount(){var map, infoWindow;
+
+  constructor(props){
+    super(props);
+    //const getCoords = this.props.getCoords;
+    this.handleLocationError = this.handleLocationError.bind(this);
+    this.state = {
+      lat: '',
+      lon: ''
+    }
+  }
+
+  getCoords() {
+    const lat = this.state.lat;
+    const lon = this.state.lon;
+    this.props.getCoords(lat, lon)
+    console.log('Pobieram coordy');
+}
+
+    componentDidMount(){
+      var map, infoWindow;
+      
     
       map = new window.google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 15
       });
-      infoWindow = new window.google.maps.InfoWindow;
+     
 
       // Try HTML5 geolocation.
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log('this inside of geo', this);
           var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
@@ -28,51 +49,74 @@ import {geolocated} from 'react-geolocated';
             map: map,
             draggable:true,
             title: 'Hello World!'})
+          
+            this.setState({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude
+            })
 
+            this.getCoords();
+            
+
+
+            document.getElementById('test').innerHTML = '<p>Want to export: Current Lat: ' + position.coords.latitude + ' Current Lng: ' + position.coords.longitude + '</p>';
+            //console.log('dzialaj dziwko', this.state.lat);
         //   infoWindow.setPosition(pos);
         //   infoWindow.setContent('Location found.');
         //   infoWindow.open(map);
           map.setCenter(pos);
 
                   //marker listeners
-                  window.google.maps.event.addListener(marker, 'dragstart', function() {
-                    console.log('marker dotkniety');
-                    document.getElementById('test').innerHTML = '<p>Currently dragging marker...</p>';
-                });
+          window.google.maps.event.addListener(marker, 'dragstart', function() {
+              console.log('marker dotkniety');
+              document.getElementById('test').innerHTML = '<p>Currently dragging marker...</p>';
+          });
         
-                window.google.maps.event.addListener(marker, 'dragend', function(e) {
-                   console.log('marker upuszczony');
-                   document.getElementById('test').innerHTML = '<p>Do wyeksportowania: Current Lat: ' + e.latLng.lat().toFixed(3) + ' Current Lng: ' + e.latLng.lng().toFixed(3) + '</p>'
-                });
+          
+          window.google.maps.event.addListener(marker, 'dragend', (e) => {
+            console.log('marker upuszczony');
+            document.getElementById('test').innerHTML = '<p>Want to export: Current Lat: ' + e.latLng.lat().toFixed(3) + ' Current Lng: ' + e.latLng.lng().toFixed(3) + '</p>';
+            console.log(this, 'this inside the event listener');
+            
+            this.setState((  {
+              lat: e.latLng.lat(),
+             lon: e.latLng.lng()
+           }))
 
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
+           this.getCoords();
+
+           console.log(this.state.lat, "PROSZE", this.state.lon)
+            }
+          );
+
+          
+        }, () => {
+          this.handleLocationError(true, map.getCenter());
         });
 
-
+        
 
       } else {
         // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        this.handleLocationError(false, map.getCenter());
       }
-    
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                            'Error: The Geolocation service failed.' :
-                            'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
-
-
+    console.log('DID MOUNT');
+    console.log(this);
+    console.log(this.state);
 }
 
+handleLocationError(browserHasGeolocation, pos, map) {
+  // infoWindow.setPosition(pos);
+  // infoWindow.setContent(browserHasGeolocation ?
+  //                       'Error: The Geolocation service failed.' :
+  //                       'Error: Your browser doesn\'t support geolocation.');
+  // infoWindow.open(map);
+}
 
     render(){
-
-        
+      
         return(
+          
             <div>
                 <div id='map' />
                 <div id='test' />
--- src/components/selectedDrugs.js
+++ src/components/selectedDrugs.js
@@ -1,18 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 
-//style dla modala
-const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
-    }
-  };
 
 export default  ({drugs, picked, deleteDrug, sendDrugs}) => {
     
